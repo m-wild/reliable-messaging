df467a19c1031869ccc32bed5ef8c78326778fef
Michael Wildman <michael.wildman@vocusgroup.co.nz>
2019-02-13 13:14:10 +1300

    remove unused variable

--- api/Startup.cs
+++ api/Startup.cs
@@ -34,7 +34,7 @@ namespace Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
             app.UseDeveloperExceptionPage();
             app.UseMvc();
